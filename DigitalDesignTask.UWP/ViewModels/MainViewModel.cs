fae1da62812cf1087f461f1d9db61b00986ee927
nomadedge <nomad.edge@gmail.com>
2019-11-14 15:49:12 +0300

    Add project files.

---
+++ DigitalDesignTask.UWP/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using DigitalDesignTask.UWP.Base;
+
+namespace DigitalDesignTask.UWP.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        private string _text;
+        private string _dataMatrixText;
+
+        public string Text
+        {
+            get { return _text; }
+            set { Set(ref _text, value); }
+        }
+
+        public string DataMatrixText
+        {
+            get { return _dataMatrixText; }
+            set { Set(ref _dataMatrixText, value); }
+        }
+
+        public void GenerateDataMatrix()
+        {
+            DataMatrixText = Text;
+        }
+    }
+}
